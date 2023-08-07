c93444ce307e02d349722ead3c92f2566577b79c
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-08-07 15:08:11 +0530

    Update equaloty.js

---
+++ lecture 5/equaloty.js
@@ -0,0 +1,11 @@
+console.log(1==1);
+console.log(1=="1");
+console.log(1==1.0);
+Console.log("" ==0);
+Console.log(0==[]);
+Console.log("" ==[]);
+Console.log(" "==0);
+Console.log(""==[]);
+Console.log(" "==0);
+Console.log(" "=="");
+Console.log(" "=="");
