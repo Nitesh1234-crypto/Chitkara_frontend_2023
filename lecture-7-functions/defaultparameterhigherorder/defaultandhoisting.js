d67b28505dda91cd650201642b12430245f43383
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-08-09 22:31:49 +0530

    Update defaultandhoisting.js

--- lecture-7-functions/defaultparameterhigherorder/defaultandhoisting.js
+++ lecture-7-functions/defaultparameterhigherorder/defaultandhoisting.js
@@ -1,25 +1,24 @@
-
-// var a=10
-// function foo(){
-//     console.log(a);
-//     var a=20;
-//     function foo1(){
-//         a++;
-//         console.log(a);
-//         var a=30;
-//         console.log(a);
-//     }
-//     foo1();
-// }
-// foo();
-
-//Higher order function
-function outerfun(fun){
-    console.log(fun);
-    console.log("inside outerfunction"+fun());
+//default parameter
+function add(a=10,b=20){
+    return a+b;
 }
-function anotherfun(){
-    console.log("inside another fun");
+console.log(add());
+console.log(add(30));
+console.log(add(undefined,40));
+
+//hoisting question
+var a=10
+function foo(){
+    console.log(a);
+    var a=20;
+    function foo1(){
+        a++;
+        console.log(a);
+        var a=30;
+        console.log(a);
+    }
+    foo1();
 }
-// outerfun(10);
-outerfun(anotherfun);
+foo();
+
+
