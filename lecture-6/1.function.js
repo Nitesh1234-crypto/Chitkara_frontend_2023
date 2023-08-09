b631af582a15b4269c875460e81344d3dd4f1a55
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-08-09 09:19:32 +0530

    Add files via upload

---
+++ lecture-6/1.function.js
@@ -0,0 +1,24 @@
+let a=add(10,30);
+console.log(a);
+function add(a,b){
+    return a+b;
+}
+console.log(z);
+var z=10;
+
+// function sub(a,b){
+//     return a-b;
+// }
+// console.log(sub(30,20));
+// function print(){
+//     console.log("Hello world!")
+// }
+// print();
+console.log(fun(10,20));
+var fun=function(a,b){
+    return a+b;
+}
+//default parameter
+function xyz(a=10,b=20){
+    return a+b;
+}
