3ab3f18d97b839eddc93a8a822a5f7032a7bcd8d
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-08-19 19:53:28 +0530

    Update pass.js

--- curry/pass.js
+++ curry/pass.js
@@ -1,22 +1,24 @@
-// //pass by value
-// // function update(a,b,c){
-// //     c=a+b;
-// // }
-// // let a=10,b=20,c;
-// // update(a,b,c);
-// // console.log(c)
-// function update(a,b,c){
-//     c=a+b;
-//     return c
-// }
-// let a=10,b=20,c;
-// c=update(a,b,c);
-// console.log(c)
+pass by value
+function update(a,b,c){
+    c=a+b;
+}
+let a=10,b=20,c;
+update(a,b,c);
+console.log(c)
 
-// //pass by reference
-// function update1(arr){
-// arr[1]=arr[0]+arr[2];
-// }
-// let arr=[1,2,3,4,6];
-// update1(arr);
-// console.log(arr);
+
+function update(a,b,c){
+    c=a+b;
+    return c
+}
+let a=10,b=20,c;
+c=update(a,b,c);
+console.log(c)
+
+//pass by reference
+function update1(arr){
+arr[1]=arr[0]+arr[2];
+}
+let arr=[1,2,3,4,6];
+update1(arr);
+console.log(arr);
