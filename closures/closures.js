3edf0167b5c7272638f3a3081132bc6fba021f80
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-08-19 19:49:44 +0530

    Update closures.js

--- closures/closures.js
+++ closures/closures.js
@@ -63,28 +63,5 @@
 // f21();
 // f22();
 // f22();
-console.log(add(1)(2)(3)(4)(5)(6)());//currying function
-console.log(mul(1)(2)(3)(4)(5)())
-function add(value){
-    if(typeof value=="undefined") return 0;
-    function inner(nextValue){
-        if(typeof nextValue=="undefined"){
-            return value;
-        }
-        value+=nextValue;
-        return inner;
-    }
-    return inner;
-}
-function mul(value){
-    if(typeof value=="undefined") return 0;
-    function inner(nextValue){
-        if(typeof nextValue=="undefined"){
-            return value;
-        }
-        value*=nextValue;
-        return inner;
-    }
-    return inner;
-}
+
 
