aa8940537da79dc5d573fab7ad212a9a5e73321b
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-09-06 16:23:42 +0530

    Update product.js

--- localStorage/product.js
+++ localStorage/product.js
@@ -1,4 +1,12 @@
 // let productData=[];
+let productData=localStorage.getItem("products");
+if(productData.length>0){
+    productData=JSON.parse(productData);
+}else{
+    productData=[];
+}
+
+
 let myForm=document.querySelector(".myform");
 let productName=document.querySelector(".name");
 let productQuantity=document.querySelector(".quantity");
@@ -7,26 +15,18 @@ myForm.addEventListener("submit",function(ev){
     ev.preventDefault();
     console.log(ev);
     console.log(productName.value ,productQuantity.value, productPrice.value);
-    let newProduct={name:productName.value,price:productPrice.value,quantity:productQuantity.value};
-    let productData=localStorage.getItem("products");
-    if(productData.length>0){
-        productData=JSON.parse(productData);
-    }else{
-    productData=[];
-    }
+    let newProduct={name:productName.value,price:productPrice.value,quantity:productQuantity.value}; 
     productData.push(newProduct);
     localStorage.setItem("products",JSON.stringify(productData));
-    showProduct(productData);///1
-    
+    showProduct();
 })
 //pge pe data show krna hai
+function showProduct(){
 let productJson=localStorage.getItem("products");
 let productInfo=JSON.parse(productJson);
 console.log(productInfo);
 let productList=document.querySelector(".productList")
-showProduct(productInfo);
-function showProduct(productData){ //2
-    productList.innerHTML="";
+productList.innerHTML=""
 for(let i=0;i<productData.length;i++){
     let productItem=productData[i];
     console.log(productItem);
@@ -35,7 +35,11 @@ for(let i=0;i<productData.length;i++){
     <span class="productQuantity">${productItem.quantity}</span>
     <span class="productPrice">${productItem.price}</span>`
     productList.append(li);
+}
+}
+showProduct();
 
-}
-}
+
+
+
  //3
