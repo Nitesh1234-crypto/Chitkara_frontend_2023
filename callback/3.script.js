2927b9c342aacd2888f3ad44a3b59322230a626b
Nitesh1234-crypto <141627194+Nitesh1234-crypto@users.noreply.github.com>
2023-09-17 16:03:42 +0530

    Update 3.script.js

--- callback/3.script.js
+++ callback/3.script.js
@@ -1,11 +1,43 @@
-function download(URL){ //http://school.com/movie.mp4
+function download(URL,cb){ // http://chitkara.com/movie.mp4
     if(!URL.startsWith("http")){
-        throw new Error ("Inavlid url");
+        throw new Error ("Invalid URL");
     }
-    console.log("....downloading start at:"+URL)
     setTimeout(()=>{
         let downloadedfile=URL.split("/").pop();
-       console.log("downloaded file:"+downloadedfile)
+        console.log(downloadedfile);
+        cb(downloadedfile)
     },3000)
 }
-download("http://school.com/movie.mp4")
+function compress(file,cb){ //movie.mp4--->movie.zip
+    setTimeout(()=>{
+        let compressFile=file.split(".")[0]+".zip";
+        console.log(compressFile);
+        cb(compressFile);
+
+    },1000)
+
+}
+function upload(file,cb){
+    let newURL="http://chitkara.com/"+file;
+    setTimeout(()=>{
+        console.log("successfully uploaded at:"+newURL);
+        cb("done");
+    },2000)
+
+}
+
+download("http://chitkara.com/notice.pdf",function(dfile){
+    compress(dfile,function(cfile){
+        upload(cfile,function(msg){
+            console.log(msg);
+        });
+    })
+})
+
+
+
+// upload("movie.zip",function(){});
+
+// compress("movie.mp4",function(){});
+
+// download("http://chitkara.com/movie.mp4",function(){});
