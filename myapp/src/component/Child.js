b2946339e1a3502a4df92ac6e406d046fa091cc7
Nitesh <Nitesh.e21@nsut.ac.in>
2023-10-08 16:02:01 +0530

    props

---
+++ myapp/src/component/Child.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Child = (props) => {
+  return (
+    <div>
+      <h1>{props.data1}</h1>
+      <h2>{props.data2}</h2>
+    </div>
+  )
+}
+
+export default Child
